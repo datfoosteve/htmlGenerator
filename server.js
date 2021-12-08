5425dc690472e5fd1cd1d221b968f075d238b316
Stephen Puthenpurackal <datfoosteve@gmail.com>
2021-12-08 17:33:02 -0600

    basic structure

---
+++ server.js
@@ -0,0 +1,16 @@
+const Engineer = require("./lib/Engineer")
+const Manager = require("./lib/Manager")
+const Intern = require("./lib/Intern")
+
+// ASk questions
+// ASk questions for every class
+
+// store questions then pass them onto class constructors
+
+
+// object initialized through these questions
+
+// run these through the html generator . like the readme file.
+
+//returns the string of the html , save it , save it in the dist folder as index.html
+
