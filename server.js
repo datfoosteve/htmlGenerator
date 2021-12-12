41f3d0d6655e5d0a05b9f29bc07ea69ca3d921d2
Stephen Puthenpurackal <datfoosteve@gmail.com>
2021-12-12 01:04:49 -0600

    rewriting and formulating the correct way

--- server.js
+++ server.js
@@ -1,62 +1,70 @@
-const Engineer = require("./lib/Engineer")
-const Manager = require("./lib/Manager")
-const Intern = require("./lib/Intern")
+const genHTML = require('./src/htmlGen');
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 
+//Create team array
+const teamArray = [];
 
-inquirer
+
+
+// First person on team is usually the manager
+// Create mananger prompts
+
+const initManager = () => {
+  inquirer
   .prompt([
 
-    {
-        type: 'input',
-        name: 'README title',
-        message: 'What is the title of the README?',
-        default: '*Title*'
-    },
+// add prompts here
+// {
+//   type: 'input',
+//   name: 'README title',
+//   message: 'What is the title of the README?',
+//   default: '*Title*'
+// },
 
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Give a decent, thoughtout, and straight to the point description about your project',
-    },
+// {
+//   type: 'input',
+//   name: 'description',
+//   message: 'Give a decent, thoughtout, and straight to the point description about your project',
+// },
 
-    {
-      type: 'input',
-      name: 'descriptionQuestion',
-      message: 'Why did you make this?',
-    },
+// {
+// type: 'input',
+// name: 'descriptionQuestion',
+// message: 'Why did you make this?',
+// },
 
-    {
-      type: 'input',
-      name: 'Purpose',
-      message: 'What is this meant to solve?',
-    },
+// {
+// type: 'input',
+// name: 'Purpose',
+// message: 'What is this meant to solve?',
+// },
 
-    {
-      type: 'input',
-      name: 'descriptionQuestion',
-      message: 'Why did you make this?',
-    },
+// {
+// type: 'input',
+// name: 'descriptionQuestion',
+// message: 'Why did you make this?',
+// },
+
+
+
   ])
 
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-    .then((data) => {
-      const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-  
-      fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-        err ? console.log(err) : console.log('Success!')
-      );
-    });
-
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
 
 
-
-
-
-
+}
 
 
     // ASk questions
