8de5c16fe42960e05b6cba8089f7e0d8dd428fd8
Stephen Puthenpurackal <datfoosteve@gmail.com>
2021-12-12 00:38:22 -0600

    the stupid ./ bug i am dumb

--- server.js
+++ server.js
@@ -2,7 +2,64 @@ const Engineer = require("./lib/Engineer")
 const Manager = require("./lib/Manager")
 const Intern = require("./lib/Intern")
 
-// ASk questions
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+
+inquirer
+  .prompt([
+
+    {
+        type: 'input',
+        name: 'README title',
+        message: 'What is the title of the README?',
+        default: '*Title*'
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a decent, thoughtout, and straight to the point description about your project',
+    },
+
+    {
+      type: 'input',
+      name: 'descriptionQuestion',
+      message: 'Why did you make this?',
+    },
+
+    {
+      type: 'input',
+      name: 'Purpose',
+      message: 'What is this meant to solve?',
+    },
+
+    {
+      type: 'input',
+      name: 'descriptionQuestion',
+      message: 'Why did you make this?',
+    },
+  ])
+
+
+    .then((data) => {
+      const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+  
+      fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+        err ? console.log(err) : console.log('Success!')
+      );
+    });
+
+
+
+
+
+
+
+
+
+    // ASk questions
 // ASk questions for every class
 
 // store questions then pass them onto class constructors
